2bd5748d86c21649e6d32d4bb9bfe799a3ad5342
Francesco Mari <francescojjmari@gmail.com>
2024-11-22 18:25:58 +0100

    Handle duplicated subscription creation events

--- app/jobs/stripe_event_processor_job.rb
+++ app/jobs/stripe_event_processor_job.rb
@@ -10,7 +10,7 @@ class StripeEventProcessorJob < ApplicationJob
     case event.type
     when "customer.subscription.created"
       subscription = event.data.object
-      Subscription.create!(stripe_id: subscription.id)
+      Subscription.create_or_find_by(stripe_id: subscription.id)
     when "invoice.paid"
       stripe_subscription_id = event.data.object.subscription
       subscription = Subscription.find_by!(stripe_id: stripe_subscription_id)

2a4dfab67d0782bf70574a081cd1e5332e4032ef
Francesco Mari <francescojjmari@gmail.com>
2024-11-23 19:51:48 +0100

    Don't use deprecated enum with keyword arguments

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,6 +1,6 @@
 class Subscription < ApplicationRecord
   include AASM
-  enum status: { unpaid: 0, paid: 1, cancelled: 2 }
+  enum :status, { unpaid: 0, paid: 1, cancelled: 2 }
   validates :stripe_id, presence: true
   belongs_to :customer
 

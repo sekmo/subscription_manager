3830f70e61e046af42d370f245990b179f10438e
Francesco Mari <francescojjmari@gmail.com>
2024-11-23 19:52:53 +0100

    Make CancelSubscriptionJob and PaySubscriptionJob resilient to duplicated events

--- app/jobs/cancel_subscription_job.rb
+++ app/jobs/cancel_subscription_job.rb
@@ -3,6 +3,9 @@ class CancelSubscriptionJob < ApplicationJob
     event = Stripe::Event.construct_from(event_data)
     stripe_subscription_id = event.data.object
     subscription = Subscription.find_by!(stripe_id: stripe_subscription_id)
+    # Handle potential duplicated cancel subscription events
+    return if subscription.cancelled?
+
     subscription.cancel!
   end
 end

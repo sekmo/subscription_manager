98e4ae040b7e5a29b06260af56075b9b0453d64a
Francesco Mari <francescojjmari@gmail.com>
2024-11-26 23:53:07 +0100

    Protect endpoint checking stripe signature

--- app/controllers/api/v1/stripe_events_controller.rb
+++ app/controllers/api/v1/stripe_events_controller.rb
@@ -1,10 +1,25 @@
 class Api::V1::StripeEventsController < ApplicationController
+  before_action :set_payload
+  before_action :verify_stripe_signature
+
   def create
-    payload = request.body.read
-    event_data = JSON.parse(payload, symbolize_names: true)
+    event_data = JSON.parse(@payload, symbolize_names: true)
 
     StripeEventDispatcher.call(event_data)
 
     head :ok
+  end
+
+  private
+
+  def set_payload
+    @payload = request.body.read
+  end
+
+  def verify_stripe_signature
+    stripe_signature = request.env["HTTP_STRIPE_SIGNATURE"]
+    Stripe::Webhook.construct_event(@payload, stripe_signature, ENV.fetch("STRIPE_WEBHOOK_SECRET"))
+  rescue Stripe::SignatureVerificationError
+    head :bad_request
   end
 end
